f41946712b1615b560edf9f51929eda674cd07a0
Andrei Sabau <andrei@asabau.com>
2022-02-06 20:36:08 +0200

    code cleanup, fixed tsc warnings, bulkSize update

--- src/plugins/PerfNodeFetchPlugin.ts
+++ src/plugins/PerfNodeFetchPlugin.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-plusplus */
-import { NodeFetchPlugin, Resource } from '@get-set-fetch/scraper';
-import { SchemaType } from '@get-set-fetch/scraper/dist/cjs/schema/SchemaHelper';
+import { NodeFetchPlugin, Resource, SchemaType } from '@get-set-fetch/scraper';
 
 export default class PerfNodeFetchPlugin extends NodeFetchPlugin {
   static get schema() {
@@ -18,9 +17,24 @@ export default class PerfNodeFetchPlugin extends NodeFetchPlugin {
             'Accept-Encoding': 'br,gzip,deflate',
           },
         },
-        rejectUnauthorized: {
+        tlsCheck: {
           type: 'boolean',
           default: true,
+          description: 'Check server certificate, certificate and hostname match',
+        },
+        connectTimeout: {
+          type: 'number',
+          default: 10 * 1000,
+        },
+        readTimeout: {
+          type: 'number',
+          default: 20 * 1000,
+        },
+        dnsResolution: {
+          type: 'string',
+          default: 'lookup',
+          title: 'DNS Resolution',
+          description: 'Use "lookup" to take into account local configuration files like /etc/hosts. Use "resolve" to always perform dns queries over the network.',
         },
         maxResources: {
           type: 'integer',
@@ -42,11 +56,16 @@ export default class PerfNodeFetchPlugin extends NodeFetchPlugin {
     // "https://www.mock-domain.org/link-".length -> 33
     const resourceNo:number = parseInt(resource.url.substring(33, resource.url.length - 5), 10);
 
-    // every 10th link contains 10 new links
+    /*
+    every {bulkSize}th link contains bulkSize new links
+    a value of 50 simulates scraping results pages with 50 entries per page
+    */
+    const bulkSize = 50;
+
     const links:string[] = [];
-    if (resourceNo < this.opts.maxResources && (resourceNo === 1 || resourceNo % 10 === 0)) {
+    if (resourceNo < this.opts.maxResources && (resourceNo === 1 || resourceNo % bulkSize === 0)) {
       // 1st insert should take into account the initial link-1 link
-      const toAddNo = resourceNo === 1 ? 9 : 10;
+      const toAddNo = resourceNo === 1 ? bulkSize - 1 : bulkSize;
       for (let i = 1; i <= toAddNo; i += 1) {
         const linkNo = resourceNo + i;
         links.push(`<a href="link-${linkNo}.html">resource ${linkNo}</a>`);
